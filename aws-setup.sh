ded2f99a73c6be3932270d241826493c11488964
Clive Chan <cc@clive.io>
2016-03-02 16:57:40 -0500

    made corrections to aws-setup.sh

--- aws-setup.sh
+++ aws-setup.sh
@@ -3,14 +3,16 @@
 # Python: Numpy, Twisted, Tensorflow (assumes python and pip)
 # Nodejs: Node, NPM
 
+cd ~
+
 sudo yum update
-ssh-keygen
-echo "Now, scp ~/.ssh/id_rsa.pub and paste this public key into GitHub."
-read -p "Press enter to continue."
+ssh-keygen -b 4096
+read -p "Now, scp ~/.ssh/id_rsa.pub and paste this public key into GitHub. [enter]"
 
 
 # INSTALL git
 sudo yum -y install git
+read -p "Now, set up your Git committer username and email. [enter]"
 git config --global --edit
 
 
@@ -30,13 +32,15 @@ mkdir gh
 git clone git@github.com:cchan/tensorflow-aws gh
 echo "About to test Python server. Here is your current AWS IPv4 address:"
 wget -qO- http://instance-data/latest/meta-data/public-ipv4
+echo 
+echo "Make sure you open up port 80 in this ec2 instance! Also update DNS if necessary."
 sudo python gh/py/server-test.py
 
 
 # INSTALL and TEST tensorflow
 sudo pip install --upgrade https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.6.0-cp27-none-linux_x86_64.whl
-echo "Testing TensorFlow..."
-python gh/tf/tf-test.py
+read -p "About to test TensorFlow [enter]..."
+python "gh/tf/tf-test.py"
 read -p "That should've printed 'Hello, TensorFlow!' and '42'."
 
 
@@ -48,13 +52,16 @@ git tag -l
 git checkout v0.12.7
 ./configure
 make && sudo make install
+read -p "About to test node. [enter]"
+node "../gh/node/node-test.js"
+read -p "Tested node. Should've outputted 'Hello world!'. [enter]"
 cd ..
 
 
 # INSTALL npm
 git clone https://github.com/isaacs/npm.git
 cd npm
-echo "Now, add ':/usr/local/bin' to the line 'Defaults secure_path = ..."
+echo "Now, add ':/usr/local/bin' to the line 'Defaults secure_path = ...' in sudoers."
 read -p "Ready?"
 sudo nano /etc/sudoers
 sudo make install
