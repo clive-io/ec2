7163374c7846d78d1cf4f789222a256818de5e54
Clive Chan <cc@clive.io>
2016-07-11 05:23:24 -0400

    reorganize, save() callbacks redo, redbird bunyan logging off

--- router/router.js
+++ router/router.js
@@ -1,21 +1,7 @@
 var PORT = 10000;
 var SAVEFILE = "~/.router/save.json";
 
-SAVEFILE = require('expand-tilde')(SAVEFILE);
 var fs = require('fs');
-require('mkdirp')(require('path').dirname(SAVEFILE), function(err){
-  if(err){
-    console.error('Unable to access savefile: mkdirp error');
-    process.exit(1);
-  }else try{
-    fs.closeSync(fs.openSync(SAVEFILE, 'a'));
-    fs.accessSync(SAVEFILE, fs.R_OK | fs.W_OK);
-  }catch(e){
-    console.error('Unable to access savefile: ' + e);
-    process.exit(1);
-  }
-});
-
 var proxy = require('redbird')({
   port: 80,
   xfwd: false,
@@ -24,13 +10,15 @@ var proxy = require('redbird')({
     key: "~/.router/certs/default.key",
     cert: "~/.router/certs/default.crt",
     ca: ["~/.router/certs/default.ca"]
-  }
+  },
+  bunyan: false
 });
 var express = require('express');
 var app = express();
 var http =  require('http').Server(app);
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
+
 
 function exportJSON(){
   var routes = {};
@@ -41,7 +29,6 @@ function exportJSON(){
   });
   return routes;
 }
-
 function importJSON(routes){
   for(var route in routes)
     if(routes.hasOwnProperty(route) 
@@ -50,48 +37,59 @@ function importJSON(routes){
       proxy.register(route, 'http://localhost:' + routes[route]);
 }
 
+//save() will log stuff.
+//save(cb) will callback on success or failure.
+//save(err, success) will callback err on failure, success on success.
+function save(err, success){
+       if(typeof err !== "function" && typeof success !== "function"){ err = console.error; success = console.log; }
+  else if(typeof err === "function" && typeof success !== "function"){ success = err; }
+  
+  var str = JSON.stringify(exportJSON());
+  fs.writeFile(SAVEFILE, str, function(err){
+    if(err) err('save: writefile error: ' + err);
+    else success('Saved: ' + str);
+  });
+}
+function load(cb){
+  if(typeof cb !== "function") cb = console.error;
+  
+  fs.readFile(SAVEFILE, function(err, data){
+    if(err){
+      cb('load: readfile error: ' + err);
+      return;
+    }else try{
+      var obj = JSON.parse(data);
+      importJSON(obj);
+    }catch(e){
+      cb('load: JSON parse error');
+      return;
+    }
+    cb('Loaded: ' + JSON.stringify(obj));
+  });
+}
+
 app.get(['/', '/export'], function(req, res){
   res.send(JSON.stringify(exportJSON(), null, 2));
 });
 app.post('/import', function(req, res){
   //`curl -H "Content-Type: application/json" --data @FILEPATH localhost:10000/import`
   importJSON(req.body);
-  save(req,res);
+  save(res.send);
 });
 
-function save(req, res){
-  var str = JSON.stringify(exportJSON());
-  fs.writeFile(SAVEFILE, str, function(err){
-    if(err) res.send('error: ' + err);
-    else res.send('Saved: ' + str);
-  });
-}
-app.get('/save', save);
-function load(cb){
-  if(typeof cb !== "function") cb = console.log;
-  
-  fs.readFile(SAVEFILE, function(err, data){
-    if(err){
-      cb('error: ' + err);
-    }else try{
-      var obj = JSON.parse(data);
-      importJSON(obj);
-      cb('Loaded: ' + JSON.stringify(obj));
-    }catch(e){
-      cb('JSON parse error');
-    }
-  });
-}
-app.get('/load', function(req, res){
-  load(res.send);
+app.get('/save', function(req, res){
+  save(res.send);
 });
-load(); //Initial load
+app.get('/load', function(req, res){
+  save(res.send);
+});
 
 app.get(/^\/register\/([a-zA-Z0-9\.]+)\/([0-9]+)/, function(req, res){
   proxy.unregister(req.params[0]);
   proxy.register(req.params[0], 'http://localhost:' + req.params[1]);
-  res.send('Registered: http://' + req.params[0] + ' => ' + 'http://localhost:' + req.params[1] + '\r\n');
-  save(req, res);
+  save(res.write, function(){
+    res.send('Registered: http://' + req.params[0] + ' => ' + 'http://localhost:' + req.params[1] + '\r\n');
+  });
 });
 app.post(/^\/register\/([a-zA-Z0-9\.]+)\/([0-9]+)/, function(req, res){
   if(req.body.key !== undefined || req.body.cert !== undefined){
@@ -110,17 +108,35 @@ app.post(/^\/register\/([a-zA-Z0-9\.]+)\/([0-9]+)/, function(req, res){
       ca: req.body.ca
     }
   });
-  res.send('Registered: https://' + req.params[0] + ' => ' + 'http://localhost:' + req.params[1] + '\r\n');
-  save(req, res);
+  save(res.write, function(){
+    res.send('Registered: https://' + req.params[0] + ' => ' + 'http://localhost:' + req.params[1] + '\r\n');
+  });
 });
 
 app.get(/^\/unregister\/([a-zA-Z0-9\.]+)/, function(req, res){
   proxy.unregister(req.params[0]);
-  res.send('Unregistered: ' + req.params[0] + '\r\n');
-  save(req, res);
+  save(res.write, function(){
+    res.send('Unregistered: ' + req.params[0] + '\r\n');
+  });
 });
 
 http.on('listening', function(){
   console.log('listening on %s:%s', http.address().address, http.address().port);
 });
-http.listen(PORT, '127.0.0.1');
+
+//Check that ~/.router and savefile exist
+SAVEFILE = require('expand-tilde')(SAVEFILE);
+require('mkdirp')(require('path').dirname(SAVEFILE), function(err){
+  if(err){
+    console.error('Unable to access savefile: mkdirp error');
+    process.exit(1);
+  }else try{
+    fs.closeSync(fs.openSync(SAVEFILE, 'a'));
+    fs.accessSync(SAVEFILE, fs.R_OK | fs.W_OK);
+  }catch(e){
+    console.error('Unable to access savefile: ' + e);
+    process.exit(1);
+  }
+});
+load(); //Initial load
+http.listen(PORT, '127.0.0.1'); //and start the server
